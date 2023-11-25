1f6e8635a21c93741c65d00b02b2f65a7e194061
Cickruss <icaro.o.macedo@gmail.com>
2023-11-25 01:32:02 -0300

    Changes by Isadora

--- Domain/Application/UseCases/CadastrarOcorrencia/UseCaseCadastrarOcorrencia.py
+++ Domain/Application/UseCases/CadastrarOcorrencia/UseCaseCadastrarOcorrencia.py
@@ -22,8 +22,10 @@ def CadastrarOcorrencia(ocorrencia):
 
 
 
-        insertOcorrencia = f'INSERT  INTO  () VALUES ()'
+        insertOcorrencia = f'INSERT INTO occurrences (gbif_id, event_date, locality) VALUES ({gbif_id},"{event_date}","{locality}");'
+        insertTaxonomia = f'INSERT INTO taxonomy (fk_kingdom_key, fk_phylum_key, fk_class_key, fk_order_key, fk_family_key, fk_genus_key, accepted_scientific_name, fk_gbif_id) VALUES ((SELECT kingdom_key FROM kingdoms WHERE "{kingdom}" = kingdom_name), (SELECT phylum_key FROM phylums WHERE "{phylum}" = phylum_name), (SELECT class_key FROM classes WHERE "{classe}" = class_name), (SELECT order_key FROM orders WHERE "{order}" = order_name), (SELECT family_key FROM families WHERE "{family}" = family_name), (SELECT genus_key FROM genus WHERE "{genus}" = genus_name), accepted_scientific_name, fk_gbif_id);'
         cursor.execute(insertOcorrencia)
+        cursor.execute(insertTaxonomia)
         connection.commit()
         connection.close()
         __BaseReturn.SuccessDataBase()
